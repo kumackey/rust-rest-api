ff2849b3d39e2097297f3dc716fff97069035594
kumackey <kumak1t09e0@gmail.com>
2023-09-09 15:44:40 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # rust-rest-api
+
+## データベース
+
+### データベース接続
+```
+docker compose up -d
+docker compose exec postgres bash
+
+psql -U postgres
+```
+
+### 初期データ挿入
+```bash
+CREATE DATABASE rust_rest_api;
+\c rust_rest_api
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    email VARCHAR NOT NULL
+);
+INSERT INTO users (name, email) VALUES ('John', 'sampleuser@email.com');
+```
+
+## web apiサーバ立ち上げ
+```bash
+cargo run
+```
+
+http://localhost:8080/
+
---
+++ src/main.rs
@@ -0,0 +1,56 @@
+use actix_web::{App, get, HttpResponse, HttpServer, post, Responder, web};
+use tokio_postgres::NoTls;
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    match query().await {
+        Ok(s) => HttpResponse::Ok().body(s),
+        Err(e) => HttpResponse::Ok().body("failed"),
+    }
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
+async fn query() -> Result<String, tokio_postgres::Error> {
+    let (client, connection) = tokio_postgres::connect(
+        "postgresql://postgres:mysecretpassword@127.0.0.1:15432/rust_rest_api",
+        NoTls,
+    )
+        .await?;
+
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    let rows = client.query("SELECT id, name FROM users", &[]).await?;
+
+    for row in &rows {
+        let id: i32 = row.get(0);
+        let name: String = row.get(1);
+        println!("found person: {} {}", id, name);
+    }
+
+    Ok("hello".to_string())
+}
